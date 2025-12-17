0f6e83cdb930a695d2926e670caf555597f8de5a
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-12-17 10:24:04 +0530

    solved 9 patterns

--- Lec 2/pattern9.java
+++ Lec 2/pattern9.java
@@ -13,7 +13,6 @@ public class pattern9{
             }
         System.out.println();
         }
-
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 System.out.print(" ");
