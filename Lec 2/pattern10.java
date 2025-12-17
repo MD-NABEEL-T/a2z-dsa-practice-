601cfbf38e7eaf4030afcbd28da0ef79b790754f
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-12-17 11:58:24 +0530

    solved 10 patterns

---
+++ Lec 2/pattern10.java
@@ -0,0 +1,12 @@
+public class pattern8{
+    public static void main(String []args){
+        int n=5;
+        for(int i=0;i<2*n-1;i++){
+            int stars = i;
+            if (stars<n) stars= 2*n-i;
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+        }
+    }
+}
