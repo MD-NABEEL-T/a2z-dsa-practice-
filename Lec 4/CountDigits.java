f64d713a149a1d070e2a6c61028a3d8287c9197d
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-23 06:13:00 +0530

    updated

--- Lec 4/CountDigits.java
+++ Lec 4/CountDigits.java
@@ -1,14 +1,28 @@
+//Time complexxity is O(log10(N))
+
 import java.util.*;
 public class CountDigits {
-    public static void myMethod(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number bhai");
-        Integer n=sc.nextInt();
+
+    // here we use Integer because cannot invoke primitive toString();
+    public static void myMethod(Integer n){
         String s=n.toString();
         System.out.println(s.length());
-        sc.close();
+    }
+    public static void anotherWay(int n)
+    {
+        int ct=0;
+        while(n>0){
+            n=n/10;
+            ct++;
+        }
+        System.out.println(ct);
     }
     public static void main(String []args){
-        myMethod();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number bhai");
+        int n=sc.nextInt();
+        myMethod(n);
+        anotherWay(n);
+        sc.close();
     }
 }
---
+++ Lec 4/ReverseNumber.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ReverseNumber {
+    public static void main(String []args){
+        int n=234700;//7432
+        int i=n%10;
+        System.out.println(i);
+    }
+}
