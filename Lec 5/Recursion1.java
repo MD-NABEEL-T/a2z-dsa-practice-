798f9fb7e24402e6cd51e4f3c15b8590f48a3ae4
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-27 22:05:39 +0530

    updated

--- Lec 5/Recursion1.java
+++ Lec 5/Recursion1.java
@@ -1,5 +1,13 @@
 public class Recursion1{
+    public static void f(int countt){
+        if(countt>3)    return;
+        else{
+            System.out.println(countt);
+            countt++;
+            f(countt);
+        }
+    }
     public static void main(String []args){
-        
+        f(0);
     }
 }
